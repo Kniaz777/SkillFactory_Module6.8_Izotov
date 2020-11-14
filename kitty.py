15caed7a81a962ffaf44d995679148dbf1c94093
Kniaz777 <72142895+Kniaz777@users.noreply.github.com>
2020-11-14 12:13:08 +0300

    Add files via upload

---
+++ kitty.py
@@ -0,0 +1,12 @@
+from Construct_cat import Cats
+
+cat1 = Cats("Барон", "мальчик", 2)
+cat2 = Cats("Сэм", "мальчик", 2)
+
+print("Имя: ", cat1.name)
+print("Пол: ", cat1.sex)
+print("Возраст: ", cat1.age)
+
+print("Имя: ", cat2.name)
+print("Пол: ", cat2.sex)
+print("Возраст: ", cat2.age)
